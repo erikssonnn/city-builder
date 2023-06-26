6a9b3ff107bf30834aedcb4308d738f3474041ee
Carl Eriksson <carl.erikssonn@hotmail.com>
2023-06-26 20:40:46 +0200

    Created custom pathfinding behaviour

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -1,34 +1,123 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
 
 public class UnitController : MonoBehaviour {
+    [SerializeField] private float speed = 0.0f;
+    [SerializeField] private float rotSpeed = 0.0f;
+    [SerializeField] private LayerMask lm = 0;
+
     private NavMeshAgent agent = null;
-    private Vector3 targetPosition = Vector3.zero;
-    public float velMag = 0;
+    private int index = -1;
+    private float velMag = 0;
+    private readonly List<Vector3> path = new List<Vector3>();
+    private bool isCreatingPath = false;
+    private Camera cam = null;
+    private Transform t = null;
+    private const float downScale = 0.1f;
 
     private void OnDrawGizmos() {
+        if (path.Count <= 0 || index == -1) return;
         Gizmos.color = Color.magenta;
-        Gizmos.DrawCube(targetPosition, Vector3.one);
-    }
-    
-    private void Start() {
-        agent = GetComponent<NavMeshAgent>();
-        RandomizeClothes();
-        SetTargetPosition();
+        Gizmos.DrawCube(path[index], Vector3.one);
+
+        if (path.Count <= 0) return;
+        foreach (Vector3 node in path) {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(node, Vector3.one * 0.5f);
+        }
     }
 
-    private void SetTargetPosition() {
-        targetPosition = new Vector3(Random.Range(-25f, 25f), 0, Random.Range(-25f, 25f));
-        agent.SetDestination(targetPosition);
+    private void Start() {
+        t = transform;
+        cam = Camera.main;
+        if (cam == null)
+            throw new System.Exception("Could not find Main Camera!");
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+            throw new System.Exception("Could not find NavMeshAgent on " + name);
+        RandomizeClothes();
+        StartCoroutine(CreatePath(GetRandomPos()));
     }
-    
+
+    private void Update() {
+        // TODO: debug, remove
+        if (Input.GetKeyDown((KeyCode.O))) {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, lm)) {
+                StartCoroutine(CreatePath(hit.point));
+            }
+        }
+
+        // CheckStuck();
+        if (path.Count <= 0 || index == -1) return;
+
+        Vector3 lookPos = path[index] - t.position;
+        lookPos.y = 0;
+        Quaternion rot = Quaternion.LookRotation(lookPos);
+        t.rotation = Quaternion.Slerp(t.rotation, rot, Time.fixedDeltaTime * rotSpeed);
+
+        t.Translate(t.TransformDirection(Vector3.forward) * (speed * downScale * Time.fixedDeltaTime), Space.World);
+        
+        float dist = Vector3.Distance(t.position, path[index]);
+        if (dist > 0.5f) return;
+        if (index >= path.Count - 1) {
+            index = -1;
+            StartCoroutine(CreatePath(GetRandomPos()));
+            return;
+        }
+        index++;
+    }
+
+    private static Vector3 GetRandomPos() {
+        return new Vector3(Random.Range(-25f, 25f), 0, Random.Range(-25f, 25f));
+    }
+
+    private IEnumerator CreatePath(Vector3 targetPos) {
+        if (isCreatingPath) {
+            StopCoroutine(CreatePath(targetPos));
+        }
+
+        isCreatingPath = true;
+
+        index = -1;
+        if (path.Count > 0) {
+            path.Clear();
+        }
+
+        agent.enabled = true;
+        agent.SetDestination(targetPos);
+        agent.isStopped = true;
+        while (agent.pathPending) {
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (agent.pathPending || agent.path.status != NavMeshPathStatus.PathComplete) {
+            print("THIS SHOULD NOT HAPPEN RIGHT");
+            StopCoroutine(CreatePath(targetPos));
+        }
+
+        NavMeshPath agentPath = agent.path;
+        agent.enabled = false;
+
+        foreach (Vector3 node in agentPath.corners.Where(node => !path.Contains(node))) {
+            path.Add(node);
+        }
+
+        index = 0;
+        isCreatingPath = false;
+    }
+
     private void RandomizeClothes() {
         MeshRenderer ren = GetComponentInChildren<MeshRenderer>();
         Material[] materials = ren.sharedMaterials;
         Material[] newMaterials = new Material[materials.Length];
-        
+
         for (int i = 0; i < materials.Length; i++) {
             newMaterials[i] = new Material(materials[i]);
         }
@@ -38,17 +127,9 @@ public class UnitController : MonoBehaviour {
     }
 
     private void CheckStuck() {
-        if (targetPosition == Vector3.zero) return;
+        if (path.Count <= 0 || index == -1) return;
         velMag = agent.velocity.magnitude;
         if (agent.velocity.magnitude > 0.1f) return;
-        SetTargetPosition();
-    }
-
-    private void Update() {
-        CheckStuck();
-        if (targetPosition == Vector3.zero) return;
-        float dist = Vector3.Distance(transform.position, targetPosition);
-        if (dist > 0.5f) return;
-        SetTargetPosition();
+        CreatePath(GetRandomPos());
     }
 }
